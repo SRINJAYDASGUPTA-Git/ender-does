5d6a558cdbf733b3422cd59ea439b6f576bbcf9e
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2026-08-13 04:40:57 +0530

    Added Suspense Fallback for TodoList component

--- ender-does-frontend/app/(root)/todos/page.tsx
+++ ender-does-frontend/app/(root)/todos/page.tsx
@@ -1,7 +1,16 @@
-"use client";
-
+import { Suspense } from "react";
 import { TodoList } from "@/components/todos/todo-list";
 
 export default function TodosPage() {
-    return <TodoList />;
+    return (
+        <Suspense
+            fallback={
+                <div className="flex min-h-[70vh] items-center justify-center">
+                    <div className="size-8 animate-spin rounded-full border-2 border-primary border-t-transparent" />
+                </div>
+            }
+        >
+            <TodoList />
+        </Suspense>
+    );
 }
