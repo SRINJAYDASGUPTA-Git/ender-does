5a4c6469bb2a4641e92cee26abc4fce610828ed2
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2026-08-13 21:35:53 +0530

    Added Docker configs and Jenkinsfile, included calls from enderdoes.srinjaydg.in

--- ender-does-frontend/next.config.ts
+++ ender-does-frontend/next.config.ts
@@ -1,40 +1,42 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "standalone",
+
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: 'i.ibb.co',
-                port: '',
-                pathname: '/**',
+                protocol: "https",
+                hostname: "i.ibb.co",
+                port: "",
+                pathname: "/**",
             },
             {
-                protocol: 'https',
-                hostname: 'avatars.githubusercontent.com',
-                port: '',
-                pathname: '/**',
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/**",
             },
             {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '/**',
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/**",
             },
             {
-                protocol: 'https',
-                hostname: 'avatar.vercel.sh',
-                port: '',
-                pathname: '/**',
+                protocol: "https",
+                hostname: "avatar.vercel.sh",
+                port: "",
+                pathname: "/**",
             },
             {
-                protocol: 'https',
+                protocol: "https",
                 hostname: "covers.openlibrary.org",
-                port: '',
-                pathname: '/b/id/**',
-            }
+                port: "",
+                pathname: "/b/id/**",
+            },
         ],
-    }
+    },
 };
 
 export default nextConfig;
